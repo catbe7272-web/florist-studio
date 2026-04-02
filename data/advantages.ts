c2f2118c0ae1799b7b91884f6b18ed6b4107cf31
catbe7272-web <catbe7272@gmail.com>
2026-04-02 14:20:08 +0300

    Create data/advantages.ts file with company advantages

---
+++ data/advantages.ts
@@ -0,0 +1,12 @@
+// Company Advantages in Florist Studio
+const advantages = [
+    "Wide selection of fresh flowers",
+    "Customizable floral arrangements",
+    "Same-day delivery options",
+    "Expert florists with years of experience",
+    "Competitive pricing and discounts",
+    "Satisfaction guarantee",
+    "Eco-friendly packaging options",
+];
+
+export default advantages;
