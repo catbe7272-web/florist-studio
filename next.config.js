486690db8b16622097f8e3eb6a83ce8793bab089
catbe7272-web <catbe7272@gmail.com>
2026-04-02 14:17:09 +0300

    Create next.config.js for Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Add your specific configurations here
+};
+
+module.exports = nextConfig;
